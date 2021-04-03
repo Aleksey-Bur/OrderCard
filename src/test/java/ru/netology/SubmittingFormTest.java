8d55628f1d62ab4e1f0920696cc1d1c0bca6f351
Aleksey-Bur <Bearaleks@mail.ru>
2021-04-03 13:55:49 +0300

    Added Test

--- src/test/java/ru/netology/SubmittingFormTest.java
+++ src/test/java/ru/netology/SubmittingFormTest.java
@@ -2,10 +2,21 @@ package ru.netology;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SubmittingFormTest {
+
+    @Test
+    void shouldTestCorrectForm() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Петр Иванов");
+        $("[data-test-id='phone'] input").setValue("+79005557777");
+        $("[data-test-id='agreement']").click();
+        $(".button__content").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
 
     @Test
     void shouldTestNotCorrectName() {
@@ -25,14 +36,5 @@ public class SubmittingFormTest {
         $("[data-test-id='agreement']").click();
         $(".button__content").click();
         $$(".input__sub").last().shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-    }
-
-    @Test
-    void shouldTestCorrectForm() {
-        $("[data-test-id=name] input").setValue("Петр Иванов");
-        $("[data-test-id='phone'] input").setValue("+79005557777");
-        $("[data-test-id='agreement']").click();
-        $(".button__content").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
